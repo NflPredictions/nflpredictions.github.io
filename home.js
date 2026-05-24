d1784272188f7848486ce7090660ac7ef709915a
NflPredictions <nflpredictions903@gmail.com>
2026-05-24 13:11:26 -0400

    Add event listener for proposeLoan button

--- home.js
+++ home.js
@@ -7,16 +7,18 @@ let ids = {
   betting: document.getElementById('betting'),
   loans: document.getElementById('loans'),
   league: document.getElementById('league'),
+  proposeLoan: document.getElementById('proposeLoan'),
 };
 
 ids.betting.addEventListener('click', function() {
   window.location.href = 'betting';
 });
-
 ids.loans.addEventListener('click', function() {
   window.location.href = 'loans';
 });
-
 ids.league.addEventListener('click', function() {
   window.location.href = 'league';
+});
+
+ids.proposeLoan.addEventListener('click', function() {
 });
