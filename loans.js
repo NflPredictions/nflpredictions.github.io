f9cbaa346fc03bdeb349e3239a63aa042fc60da5
NflPredictions <nflpredictions903@gmail.com>
2026-05-24 13:03:38 -0400

    Add navigation and authentication to loans.js

---
+++ loans.js
@@ -0,0 +1,22 @@
+if (get('signedIn') !== true) {
+  window.location.href = 'index';
+}
+
+let ids = {
+  home: document.getElementById('home'),
+  betting: document.getElementById('betting'),
+  loans: document.getElementById('loans'),
+  league: document.getElementById('league'),
+};
+
+ids.home.addEventListener('click', function() {
+  window.location.href = 'home';
+});
+
+ids.betting.addEventListener('click', function() {
+  window.location.href = 'betting';
+});
+
+ids.league.addEventListener('click', function() {
+  window.location.href = 'league';
+});
