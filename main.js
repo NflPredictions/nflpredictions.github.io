b014588fbf7d66c13ee5780892c8bed30095f841
NflPredictions <nflpredictions903@gmail.com>
2026-05-24 15:43:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCictD2QgC4sK-GTqpFeLOiDJucCziVjeE",
+  authDomain: "nflpredictions-339cb.firebaseapp.com",
+  projectId: "nflpredictions-339cb",
+  storageBucket: "nflpredictions-339cb.firebasestorage.app",
+  messagingSenderId: "1029734879051",
+  appId: "1:1029734879051:web:a3ec1cb70edee22852a2ce",
+  measurementId: "G-S1W6397FN0"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function set(a, b) {
   localStorage.setItem(a, JSON.stringify(b));
 }
