45e9dbffe711c2a9f1cf639d31d33b18d8eaf5af
NflPredictions <nflpredictions903@gmail.com>
2026-05-26 12:40:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 import { firebaseConfig, app, analytics, set, get } from "/main.js";
 
 set('signedIn', false);
