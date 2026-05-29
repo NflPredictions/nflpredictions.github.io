db4709c72089bcd160b24537667c9ab3e8394485
NflPredictions <nflpredictions903@gmail.com>
2026-05-29 12:32:41 -0400

    Update league.js

--- league.js
+++ league.js
@@ -4,10 +4,15 @@ import { signOut } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-aut
 async function exit() {
   try {
     await signOut(auth);
+    console.log('User signed out');
     set('user', '');
-    window.location.href = '/index';
+    window.location.href = 'index';
   } catch (error) {
-    console.error(`Error signing out ${error.message}`);
+    console.error('Error signing out:', error.message);
+    ids.authRed.textContent = `Error signing out: ${error.message}`;
+    ids.headerRed.display = 'flex';
+    await sleep(3000);
+    ids.headerRed.display = 'none';
   }
 }
 
