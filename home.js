425f3d24650c035d477de8712b1f417327b02462
NflPredictions <nflpredictions903@gmail.com>
2026-05-29 12:41:42 -0400

    Update home.js

--- home.js
+++ home.js
@@ -4,15 +4,15 @@ import { signOut } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-aut
 async function exit() {
   try {
     await signOut(auth);
-    console.log('User signed out');
     set('user', '');
+    console.log('User signed out');
     window.location.href = 'index';
   } catch (error) {
-    console.error('Error signing out:', error.message);
     ids.authRed.textContent = `Error signing out: ${error.message}`;
     ids.headerRed.display = 'flex';
     await sleep(3000);
     ids.headerRed.display = 'none';
+    console.error('Error signing out:', error.message);
   }
 }
 
