9271af75c8dcb71d8fc3070e793b9bd9a2f1f459
NflPredictions <nflpredictions903@gmail.com>
2026-05-29 12:42:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ ids.signUp.addEventListener('click', async function() {
   const password = ids.password.value;
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    console.log('User signed up:', userCredential.user);
     ids.authGreen.textContent = `Signed up as: ${userCredential.user.email}`;
     ids.headerGreen.display = 'flex';
     await sleep(3000);
     ids.headerGreen.display = 'none';
     set('user', userCredential);
+    console.log('User signed up:', userCredential.user);
   } catch (error) {
-    console.error('Error signing up:', error.message);
     ids.authRed.textContent = `Error signing up: ${error.message}`;
     ids.headerRed.display = 'flex';
     await sleep(3000);
     ids.headerRed.display = 'none';
+    console.error('Error signing up:', error.message);
   }
 });
 
@@ -39,14 +39,14 @@ signIn.addEventListener('click', async function() {
   const password = ids.password.value;
   try {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    console.log('User signed in:', userCredential.user);
     set('user', userCredential);
+    console.log('User signed in:', userCredential.user);
     window.location.href = 'leagues';
   } catch (error) {
-    console.error('Error signing in:', error.message);
     ids.authRed.textContent = `Error signing in: ${error.message}`;
     ids.headerRed.display = 'flex';
     await sleep(3000);
     ids.headerRed.display = 'none';
+    console.error('Error signing in:', error.message);
   }
 });
