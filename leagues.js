249370441bf3f4c83dc3809479dfed2c9bcd31f0
NflPredictions <nflpredictions903@gmail.com>
2026-05-30 14:42:56 -0400

    Update leagues.js

--- leagues.js
+++ leagues.js
@@ -20,9 +20,12 @@ let ids = {
   createLeague: document.getElementById('createLeague'),
   joinLeague: document.getElementById('joinLeague'),
   exit: document.getElementById('exit'),
+  headerRed: document.getElementById('headerRed'),
+  authRed: document.getElementById('authRed'),
 };
 
 ids.createLeague.addEventListener('click', function() {
+  window.location.href = 'createLeague';
 });
 ids.joinLeague.addEventListener('click', function() {
 });
