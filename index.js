2137a19cf55a701918c42be6ffd1506b2b43d224
NflPredictions <nflpredictions903@gmail.com>
2026-05-30 14:20:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 import { auth, set, get, sleep } from "/main.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const db = getFirestore();
 
 set('user', '');
 
@@ -14,6 +17,17 @@ let ids = {
   authRed: document.getElementById('authRed'),
 };
 
+
+async function createUser(userID, userData) {
+  try {
+    const docRef = doc(db, 'users', userID);
+    await setDoc(docRef, userData);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error.message);
+  }
+}
+
 ids.signUp.addEventListener('click', async function() {
   const email = ids.email.value;
   const password = ids.password.value;
@@ -21,6 +35,13 @@ ids.signUp.addEventListener('click', async function() {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     console.log('User signed up:', userCredential.user);
     set('user', userCredential);
+
+    const userData = {
+      name: userCredential.user.email.substring(userCredential.user.email.indexOf('@')),
+      leagues: {league: {}},
+    };
+    await createUser(userCredential.user.uid, userData);
+    
     ids.authGreen.textContent = `Signed up as: ${userCredential.user.email}`;
     ids.headerGreen.classList.add('show');
     await sleep(3000);
