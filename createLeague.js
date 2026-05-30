c1506e30f55134af0e782ea8e154f108f4c8032c
NflPredictions <nflpredictions903@gmail.com>
2026-05-30 15:05:01 -0400

    Update createLeague.js

--- createLeague.js
+++ createLeague.js
@@ -1,1 +1,13 @@
 
+
+const ids = {
+  name: document.getElementById('name'),
+  password: document.getElementById('password'),
+  cash: document.getElementById('cash'),
+  createLeague: document.getElementById('createLeague'),
+  exit: document.getElementById('exit'),
+};
+
+async function createLeague() {
+
+}
