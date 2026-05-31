53d8a5b76cfa16307b03816d512da960ed37ed57
NflPredictions <nflpredictions903@gmail.com>
2026-05-31 14:59:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,61 +15,6 @@ let ids = {
   authRed: document.getElementById('authRed'),
 };
 
-
-async function createUser(userID, userData) {
-  try {
-    const docRef = doc(db, 'users', userID);
-    await setDoc(docRef, userData);
-    console.log("Document written with ID: ", docRef.id);
-  } catch (error) {
-    console.error("Error adding document: ", error.message);
-  }
-}
-
-async function signUp(ids) {
-  const email = ids.email.value;
-  const password = ids.password.value;
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    console.log('User signed up:', userCredential.user);
-    set('user', userCredential);
-
-    const userData = {
-      name: userCredential.user.email.substring(0, userCredential.user.email.indexOf('@')),
-      leagues: {league: {}},
-    };
-    await createUser(userCredential.user.uid, userData);
-    
-    ids.authGreen.textContent = `Signed up as: ${userCredential.user.email}`;
-    ids.headerGreen.classList.add('show');
-    await sleep(3000);
-    ids.headerGreen.classList.remove('show');
-  } catch (error) {
-    console.error('Error signing up:', error.message);
-    ids.authRed.textContent = `Error signing up: ${error.message}`;
-    ids.headerRed.classList.add('show');
-    await sleep(3000);
-    ids.headerRed.classList.remove('show');
-  }
-}
-
-async function signIn(ids) {
-  const email = ids.email.value;
-  const password = ids.password.value;
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    set('user', userCredential);
-    console.log('User signed in:', userCredential.user);
-    window.location.href = 'leagues';
-  } catch (error) {
-    console.error('Error signing in:', error.message);
-    ids.authRed.textContent = `Error signing in: ${error.message}`;
-    ids.headerRed.classList.add('show');
-    await sleep(3000);
-    ids.headerRed.classList.remove('show');
-  }
-}
-
 ids.signUp.addEventListener('click', async function() {
   signUp(ids);
 });
